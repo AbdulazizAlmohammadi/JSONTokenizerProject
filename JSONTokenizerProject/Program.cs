581f7f6fafe542d671bc71694d4743023ea3fdc8
Abdulaziz Almohammadi <AbdulazizAlmohammadi@users.noreply.github.com>
2021-04-25 03:55:42 +0300

    edit number handler

--- JSONTokenizerProject/Program.cs
+++ JSONTokenizerProject/Program.cs
@@ -9,23 +9,23 @@ namespace JSONTokenizerProject
         static void Main(string[] args)
         {
 
-            JSON json = new JSON("{\n" +
-                "   \"hello\": \"arr\",\n" +
-                "   \"age\":[\n " +
-                "       \"hh\" , 12 , [\n" +
-                "        1 ,\n " +
-                "       {\"key\":\"value\"}\n" +
-                "                    ]\n" +
-                "         ], \n" +
-                "   \"ismarried\" : true\n" +
-                "}");
+            //JSON json = new JSON("{\n" +
+            //    "   \"hello\": \"arr\",\n" +
+            //    "   \"age\":[\n " +
+            //    "       \"hh\" , 12 , [\n" +
+            //    "        1 ,\n " +
+            //    "       {\"key\":\"value\"}\n" +
+            //    "                    ]\n" +
+            //    "         ], \n" +
+            //    "   \"ismarried\" : true\n" +
+            //    "}");
 
-<<<<<<< HEAD
+
             //JSON json = new JSON(@" [ 1 , 2 , {""ke5y"" : 55 }, {""TEST"" : {""INSIDE"": [4,{""ARR-_23"":9},4]}} , {""EXAMPLE"" : nuLL} ]");
             //JSON json = new JSON(@" true");
-=======
+
             JSON json = new JSON(@"{""key"": 1.7e+3 }");
->>>>>>> 9bcb91cddf28feb615b17cc46bb8a4c793b600ab
+
 
             //JSON json1 = new JSON("false");
             //JSON json2 = new JSON("12");
@@ -39,20 +39,20 @@ namespace JSONTokenizerProject
                 JBool jbool = (JBool)js;
                 Console.WriteLine(jbool.value);
 
-<<<<<<< HEAD
+
             } else if(js is JArray)
             {
                 JArray arr = (JArray)js;
             } else if(js is JObject)
             {
                 JObject obj = (JObject)js;
-                Console.WriteLine(obj.value[1].key);
+                Console.WriteLine(obj.value[0].key);
             }
-=======
-            json.getObject();
+
+          //  json.getObject();
 
 
->>>>>>> 9bcb91cddf28feb615b17cc46bb8a4c793b600ab
+
         }
     }
 }
